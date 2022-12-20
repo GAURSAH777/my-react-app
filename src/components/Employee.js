e4b5a28f34d450d24669d0218e036cf89cb5add8
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-12-20 14:57:41 +0530

    added counter using functional component

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 
 export const Employee = (props) => {
@@ -6,12 +6,33 @@ export const Employee = (props) => {
         name: PropTypes.string.isRequired,
         id: PropTypes.number.isRequired
     }
+    const [count,setCount]=useState(0);
+
+    function increment(){
+        setCount(count + 1);
+    }
+
+    function decrement(){
+        // if count is less or equal to zero then return 0 else count-1  ----> ternary operation 
+        setCount(count <= 0 ? 0 : count-1);
+    }
+
+    function reset(){
+        setCount(0);
+    }
+
   return (
     <div>
         Employee Details
         <h3>Welcome {props.name}</h3>
         <h3> Employee ID: {props.id}</h3>
+        <button onClick={increment}>+</button>
+        <span>Count: {count}</span>
+        <button onClick={decrement}>-</button>
+        <div style={{textAlign: 'Center'}}>
+        <button onClick={reset}>Reset</button>
+        </div>
     </div>
 
-  )
+  );
 }
